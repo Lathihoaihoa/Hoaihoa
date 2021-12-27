09bfb47764d1ad9550eda40b14e1e5620e26da33
Lathihoaihoa <63432141+Lathihoaihoa@users.noreply.github.com>
2021-12-27 23:27:27 +0700

    Add files via upload

---
+++ views/template-parts/page/module-content_image.blade.php
@@ -0,0 +1,34 @@
+<section class="mod-content-image bg-greenwhite mb-95 xl:mb-42 2xl:mb-34">
+    <div class="container">
+        <div class="mx-auto max-w-646 xl:max-w-full 2k:-ml-2">
+            <div class="row">
+                <div class="col max-w-576 ml-33 xl:max-w-360 xl:ml-36 xl:mt-35
+                2xl:max-w-600 2xl:ml-30 2xl:mt-40">
+                   <h3>
+                    Our Open Ecosystem of Partners Gives You the Power to Choose
+                   </h3>
+                   <p>
+                    We believe in the power of transparency and connectedness. Encompass’ 
+                    ecosystem of partners combines diversity and best-in-class API 
+                    functionality to provide one powerful, unified source of truth for 
+                    our clients, no matter how many modules they choose to utilize. 
+                   </p>
+                    <p>
+                    With Encompass ERP, you choose only the tools and partners you need, 
+                    leaving out what isn’t necessary.  Finally, your beverage or food supply 
+                    chain can now be managed with a single sign-on experience without 
+                    compromising efficiency or breaking data flow.
+                   </p>
+                </div>
+        
+                <div class="col partners-slider max-w-646 ml-18 
+                xl:max-w-470 xl:mt-109 2xl:max-w-606
+                2xl:ml-10 2xl:mt-27 w-full">
+                <img src="@asset('images/emcompass/logoall.png')" alt="Logo" class="w-full">
+                    
+                    <div class="arrow-append1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
